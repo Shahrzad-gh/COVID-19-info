1c49775bf01e1f95bc4224e02c8345b81c136e06
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-05-21 17:21:22 +0430

    Updates

---
+++ precache-manifest.42f182215f2fc27144909e94cd008461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f34fd016a41ad192485cd9fbc3dba5",
+    "url": "/COVID-19-info/index.html"
+  },
+  {
+    "revision": "589dfd45aa953faa06db",
+    "url": "/COVID-19-info/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0d4a92ea09c6d20844e4",
+    "url": "/COVID-19-info/static/js/2.7e94b969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/COVID-19-info/static/js/2.7e94b969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589dfd45aa953faa06db",
+    "url": "/COVID-19-info/static/js/main.4ab42af7.chunk.js"
+  },
+  {
+    "revision": "ba274bd647c242e14369",
+    "url": "/COVID-19-info/static/js/runtime-main.3f113603.js"
+  }
+]);
