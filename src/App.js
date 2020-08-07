e8ebeeb4081b5ba6677936a0e1a11d9a7c843c00
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-08-07 21:40:46 +0430

    some update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
-import  SearchForm  from './Components/SearchForm';
-import {store} from './Store/store';
-import {Provider} from 'react-redux'
-import  CountryContainer  from './Components/CountryContainer';
+import React from "react";
+import "./App.css";
+import SearchForm from "./Components/SearchForm";
+import { store } from "./Store/store";
+import { Provider } from "react-redux";
+import CountryContainer from "./Components/CountryContainer";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <Provider store={store}>
       <SearchForm />
-      <CountryContainer />
+      <CountryContainer /> 
+      {/* <Dashboard /> */}
     </Provider>
   );
 }
--- src/Components/CountryContainer.js
+++ src/Components/CountryContainer.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import Country from './Country';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Country from "./Country";
 
 export class CountryContainer extends Component {
-    render() {
-        const {data} = this.props;
-        let content ='';
-        content = data.lenght !== 0 
-        ? data.map((country,index) => <Country key={index} country={country} />)
+  render() {
+    const { data } = this.props;
+    let content = "";
+    content =
+      data.lenght !== 0
+        ? data.map((country, index) => (
+            <Country key={index} country={country} />
+          ))
         : null;
-        return (
-            <div>
-                {content}
-            </div>
-        )
-    }
+    return <div>{content}</div>;
+  }
 }
-const mapStateToProps = state => ({
-    data : state.country.data
-})
+const mapStateToProps = (state) => ({
+  data: state.country.data,
+});
 
 export default connect(mapStateToProps)(CountryContainer);
